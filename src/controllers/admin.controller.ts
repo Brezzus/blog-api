b2c6745975c2a7680251e5a5e295f10342278843
Breschi <breschicaua426@gmail.com>
2026-06-01 08:35:16 -0300

    criando endpoints

---
+++ src/controllers/admin.controller.ts
@@ -0,0 +1,22 @@
+import { RequestHandler } from "express";
+
+
+export const addPost: RequestHandler = async (req, res) => {
+
+}
+
+export const editPost: RequestHandler = async (req, res) => {
+    
+}
+
+export const deletePost: RequestHandler = async (req, res) => {
+    
+}
+
+export const getAllPosts: RequestHandler = async (req, res) => {
+    
+}
+
+export const getPost: RequestHandler = async (req, res) => {
+    
+}
