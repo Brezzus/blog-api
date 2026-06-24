505cc7e7d6defd3cbc5c03224ec3d9210d0114ba
Breschi <breschicaua426@gmail.com>
2026-06-24 08:36:00 -0300

    iniciando cadastro do post

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -1,8 +1,29 @@
-import { RequestHandler } from "express";
+import { RequestHandler, Response } from "express";
+import { ExtendedRequest } from "../types/extended-request";
+import z from "zod";
+import { getUserById } from "../services/user.service";
+import { error } from "node:console";
+import { handleCover } from "../services/post.service";
 
+export const addPost: RequestHandler = async (req: ExtendedRequest, res: Response) => { if (req!.user) return
 
-export const addPost: RequestHandler = async (req, res) => {
+    const schema = z.object ({
+        title: z.string(),
+        tags: z.string(),
+        body: z.string()
+    })
+    const data = schema.safeParse(req.body)
+    if(!data.success) {
+        return res.status(400).json({ error: data.error.flatten().fieldErrors })
+    }
+    if (!req.file) {
+        return res.status(400).json({error:'image is required'})
+    }
 
+    const coverName = await handleCover(req.file)
+    if(!coverName) {
+        return res.status(400).json({ error:'invalid cover image' })
+    }
 }
 
 export const editPost: RequestHandler = async (req, res) => {
